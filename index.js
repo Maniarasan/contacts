23a8902d6750f3d401246f39a16fc34746d462a4
intern5 <intern5@abcoind.local>
2016-05-13 18:08:52 +0530

    port change

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use(function(req, res, next) {
 var mail_route='/myaction/:email/:password';
 app.post(mail_route, mail);
 
-app.listen($PORT, function() {
+app.listen(process.env.PORT || 80, function() {
   console.log('Server running at http://127.0.0.1:3000/');
 });
